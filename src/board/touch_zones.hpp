d78a52be348e96b8045a21335835e8d729f87b63
tom <261801702+techrote@users.noreply.github.com>
2026-09-14 23:40:50 +0100

    ES-003: add ESP32-S3 touch-zone board adapter

---
+++ src/board/touch_zones.hpp
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <array>
+#include <cstddef>
+#include <cstdint>
+
+#include <espsand/input/touch_normalizer.hpp>
+#include <espsand/io/interfaces.hpp>
+
+namespace espsand::board {
+
+class TouchZones final : public io::ITouchZones {
+public:
+  bool begin();
+  bool poll(std::uint64_t now_us, io::TouchFrame& frame) override;
+  io::TouchStatus status() const override;
+  io::TouchDiagnostics diagnostics() const override;
+
+private:
+  static constexpr std::uint64_t kChannelPeriodUs = 4000;
+
+  float group_max(const std::array<std::size_t, 3>& indices) const;
+  void update_provisional_groups(io::TouchFrame& frame, std::uint64_t now_us);
+
+  input::TouchNormalizer normalizer_{};
+  input::TouchZoneGate zone_a_gate_{};
+  input::TouchZoneGate zone_b_gate_{};
+  input::TouchZoneGate combo_gate_{};
+  std::array<std::uint32_t, io::kMaxTouchChannels> raw_{};
+  io::TouchDiagnostics diagnostics_{};
+  io::TouchStatus status_{};
+  std::size_t next_channel_ = 0;
+  std::uint64_t next_channel_us_ = 0;
+};
+
+} // namespace espsand::board
