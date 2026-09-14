949471211d9ee5650b9c69112694fe254bdacb44
tom <261801702+techrote@users.noreply.github.com>
2026-09-14 23:41:46 +0100

    ES-003: integrate touch adapter into diagnostic runtime

--- src/app/diagnostic_runtime.hpp
+++ src/app/diagnostic_runtime.hpp
@@ -2,6 +2,7 @@
 
 #include <cstdint>
 
+#include <espsand/io/touch_types.hpp>
 #include <espsand/io/types.hpp>
 #include <espsand/runtime/diagnostic_controller.hpp>
 
@@ -10,6 +11,7 @@
 #include "board/monotonic_clock.hpp"
 #include "board/qmi8658_imu.hpp"
 #include "board/serial_diagnostics.hpp"
+#include "board/touch_zones.hpp"
 
 namespace espsand::app {
 
@@ -23,21 +25,26 @@ private:
   void render_pixel_sweep(io::Frame8x8& frame, std::uint32_t elapsed_ms) const;
   void render_primary_colours(io::Frame8x8& frame, std::uint32_t elapsed_ms) const;
   void render_gravity(io::Frame8x8& frame) const;
+  void render_touch_characterization(io::Frame8x8& frame) const;
   void overlay_fault(io::Frame8x8& frame, std::uint32_t elapsed_ms) const;
   void emit_button_event(io::ButtonEvent event);
   void emit_telemetry(std::uint64_t now_us);
+  void emit_touch_telemetry(std::uint64_t now_us);
 
   board::MonotonicClock clock_{};
   board::MatrixOutput matrix_{};
   board::Qmi8658Imu imu_{};
   board::BootButton button_{};
+  board::TouchZones touch_{};
   board::SerialDiagnostics diagnostics_{};
   runtime::DiagnosticController controller_{};
   io::ImuSample latest_imu_{};
+  io::TouchFrame latest_touch_{};
 
   std::uint64_t next_imu_us_ = 0;
   std::uint64_t next_render_us_ = 0;
   std::uint64_t next_telemetry_us_ = 0;
+  std::uint64_t next_touch_telemetry_us_ = 0;
   std::uint64_t telemetry_window_start_us_ = 0;
   std::uint64_t max_loop_us_ = 0;
   std::uint32_t imu_samples_window_ = 0;
