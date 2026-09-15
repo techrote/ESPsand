6b4970ed7f2c416edee9b8fea21013634e1382f4
tom <261801702+techrote@users.noreply.github.com>
2026-09-15 22:38:56 +0100

    Map Moss slider to spatial rain

--- lib/espsand_core/include/espsand/sim/moss_garden_scene.hpp
+++ lib/espsand_core/include/espsand/sim/moss_garden_scene.hpp
@@ -35,10 +35,8 @@ struct MossGardenSceneStats {
   std::uint16_t feeds = 0;
   std::uint16_t starved = 0;
   std::uint16_t rain_pulses = 0;
-  union {
-    std::uint16_t seed_pulses = 0;
-    std::uint16_t touch_mite_spawns;
-  };
+  std::uint16_t touch_rain_pulses = 0;
+  std::uint16_t seed_pulses = 0;
   std::uint16_t scatter_events = 0;
 };
 
