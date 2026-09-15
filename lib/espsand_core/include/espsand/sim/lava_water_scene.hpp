04375ddbbe581604a17da61d85bc015c4b7c9fc8
tom <261801702+techrote@users.noreply.github.com>
2026-09-15 21:32:12 +0100

    Alias Lava touch counter for hash compatibility

--- lib/espsand_core/include/espsand/sim/lava_water_scene.hpp
+++ lib/espsand_core/include/espsand/sim/lava_water_scene.hpp
@@ -14,7 +14,10 @@ inline constexpr std::uint32_t kLavaWaterSceneSchemaVersion = 4;
 struct LavaWaterSceneStats {
   std::uint16_t autonomous_lava_injections = 0;
   std::uint16_t autonomous_water_injections = 0;
-  std::uint16_t touch_water_injections = 0;
+  union {
+    std::uint16_t touch_water_injections = 0;
+    std::uint16_t touch_lava_injections;
+  };
   std::uint16_t burst_pairs = 0;
   std::uint16_t crust_fractures = 0;
   std::uint8_t last_injection_x = 0;
