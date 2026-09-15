cb7fa56bbf0de7d6f6bbbf8cb270b5795e0f9e1f
tom <261801702+techrote@users.noreply.github.com>
2026-09-15 21:32:32 +0100

    Keep Moss spawn counter hash-compatible

--- lib/espsand_core/include/espsand/sim/moss_garden_scene.hpp
+++ lib/espsand_core/include/espsand/sim/moss_garden_scene.hpp
@@ -36,7 +36,6 @@ struct MossGardenSceneStats {
   std::uint16_t starved = 0;
   std::uint16_t rain_pulses = 0;
   std::uint16_t seed_pulses = 0;
-  std::uint16_t touch_mite_spawns = 0;
   std::uint16_t scatter_events = 0;
 };
 
