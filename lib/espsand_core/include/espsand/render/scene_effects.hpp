ec30b1cf1db575bf28dfb3aea9cea10a5d9f411f
tom <261801702+techrote@users.noreply.github.com>
2026-09-15 07:30:32 +0100

    render: keep scene effects focused on temporal overlays

--- lib/espsand_core/include/espsand/render/scene_effects.hpp
+++ lib/espsand_core/include/espsand/render/scene_effects.hpp
@@ -4,11 +4,8 @@
 
 #include <espsand/io/types.hpp>
 #include <espsand/sim/moss_garden_scene.hpp>
-#include <espsand/sim/world.hpp>
 
 namespace espsand::render {
-
-void apply_structural_contrast(io::Frame8x8& frame, const sim::World& world) noexcept;
 
 void blend_with_previous(io::Frame8x8& frame, const io::Frame8x8& previous,
                          std::uint8_t previous_weight_q8) noexcept;
