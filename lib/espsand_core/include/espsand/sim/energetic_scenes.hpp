a8dc5f77bc32517b83974ff8458bc2da45b6b42f
tom <261801702+techrote@users.noreply.github.com>
2026-09-15 21:29:33 +0100

    Retarget energetic touch controls to secondary material

--- lib/espsand_core/include/espsand/sim/energetic_scenes.hpp
+++ lib/espsand_core/include/espsand/sim/energetic_scenes.hpp
@@ -9,8 +9,8 @@
 
 namespace espsand::sim {
 
-inline constexpr std::uint32_t kSodiumWaterSceneSchemaVersion = 3;
-inline constexpr std::uint32_t kOilFireSceneSchemaVersion = 3;
+inline constexpr std::uint32_t kSodiumWaterSceneSchemaVersion = 4;
+inline constexpr std::uint32_t kOilFireSceneSchemaVersion = 4;
 
 struct EnergeticSceneTickContext {
   World& world;
@@ -23,7 +23,7 @@ struct EnergeticSceneTickContext {
 struct SodiumWaterSceneStats {
   std::uint16_t autonomous_sodium_injections = 0;
   std::uint16_t autonomous_water_injections = 0;
-  std::uint16_t touch_sodium_injections = 0;
+  std::uint16_t touch_water_injections = 0;
   std::uint16_t burst_pairs = 0;
   std::uint8_t last_injection_x = 0;
 };
@@ -31,7 +31,7 @@ struct SodiumWaterSceneStats {
 struct OilFireSceneStats {
   std::uint16_t autonomous_oil_injections = 0;
   std::uint16_t autonomous_ignitions = 0;
-  std::uint16_t touch_oil_injections = 0;
+  std::uint16_t touch_ignitions = 0;
   std::uint16_t combo_ignitions = 0;
   std::uint8_t last_injection_x = 0;
 };
