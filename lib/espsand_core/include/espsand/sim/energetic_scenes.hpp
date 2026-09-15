e3a0561dd20b9f0435a2c805d8ba9b802fce1745
tom <261801702+techrote@users.noreply.github.com>
2026-09-15 21:32:21 +0100

    Alias energetic touch counters for hash compatibility

--- lib/espsand_core/include/espsand/sim/energetic_scenes.hpp
+++ lib/espsand_core/include/espsand/sim/energetic_scenes.hpp
@@ -23,7 +23,10 @@ struct EnergeticSceneTickContext {
 struct SodiumWaterSceneStats {
   std::uint16_t autonomous_sodium_injections = 0;
   std::uint16_t autonomous_water_injections = 0;
-  std::uint16_t touch_water_injections = 0;
+  union {
+    std::uint16_t touch_water_injections = 0;
+    std::uint16_t touch_sodium_injections;
+  };
   std::uint16_t burst_pairs = 0;
   std::uint8_t last_injection_x = 0;
 };
@@ -31,7 +34,10 @@ struct SodiumWaterSceneStats {
 struct OilFireSceneStats {
   std::uint16_t autonomous_oil_injections = 0;
   std::uint16_t autonomous_ignitions = 0;
-  std::uint16_t touch_ignitions = 0;
+  union {
+    std::uint16_t touch_ignitions = 0;
+    std::uint16_t touch_oil_injections;
+  };
   std::uint16_t combo_ignitions = 0;
   std::uint8_t last_injection_x = 0;
 };
